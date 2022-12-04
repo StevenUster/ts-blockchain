f20b6f7fe7637f7b639bdc91248abac8ba9502bc
StevenUster <stevenelias@web.de>
2022-12-04 17:27:20 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,5 @@ docker build -t blockchain-backend ./backend
 
 docker run -d --name blockchain-frontend --restart on-failure:5 -p 80:80 blockchain-frontend
 docker run -d --name blockchain-backend --restart on-failure:5 -p 3000:3000 blockchain-backend
+
+docker logs -f blockchain-backend
